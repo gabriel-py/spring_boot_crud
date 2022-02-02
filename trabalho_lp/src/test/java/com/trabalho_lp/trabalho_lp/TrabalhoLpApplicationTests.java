5f0de220aead1fb87644a21383433b5489afe124
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2022-02-02 09:23:13 -0300

    Alterado pom.xml

--- trabalho_lp/src/test/java/com/trabalho_lp/trabalho_lp/TrabalhoLpApplicationTests.java
+++ trabalho_lp/src/test/java/com/trabalho_lp/trabalho_lp/TrabalhoLpApplicationTests.java
@@ -1,10 +1,24 @@
 package com.trabalho_lp.trabalho_lp;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.trabalho_lp.trabalho_lp.controller.ClienteController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TrabalhoLpApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	private ClienteController clienteController;
 
 	@Test
 	void contextLoads() {
